2d323ca19f2bf07eb1456243731e72fcd9ba99ad
Mathieu Bastian <mathieu.bastian@gmail.com>
2025-11-11 14:07:03 +0100

    Add position to generator

--- src/main/java/org/gephi/graphstore/benchmark/util/Generator.java
+++ src/main/java/org/gephi/graphstore/benchmark/util/Generator.java
@@ -6,6 +6,7 @@ import java.util.Set;
 import org.gephi.graph.api.Configuration;
 import org.gephi.graph.api.Edge;
 import org.gephi.graph.api.GraphFactory;
+import org.gephi.graph.api.Node;
 import org.gephi.graph.impl.GraphModelImpl;
 import org.gephi.graph.impl.GraphStore;
 import org.gephi.graph.impl.NodeImpl;
@@ -14,11 +15,14 @@ import org.gephi.graphstore.benchmark.draft.DraftNode;
 
 public class Generator {
 
+    public static final float MINIMUM_POSITION = -10000;
+    public static final float MAXIMUM_POSITION = 10000;
     protected final DraftNode[] nodes;
     protected final DraftEdge[] edges;
     protected Configuration configuration;
     protected boolean withNodes = true;
     protected boolean withEdges = true;
+    protected boolean withPositions = false;
 
     private Generator(int nodes, int edges, int types) {
         this.nodes = new DraftNode[nodes];
@@ -39,30 +43,18 @@ public class Generator {
     }
 
     private static DraftEdge[] generateRandomEdges(DraftNode[] nodes, int edgeCount, int typeCount) {
-        double wiringProbability = ((double) edgeCount) / (nodes.length * (nodes.length - 1));
         Random random = new Random();
         Set<DraftEdge> edgeSet = new HashSet<>();
         while (edgeSet.size() < edgeCount) {
-            for (int i = 0; i < nodes.length - 1; i++) {
-                DraftNode source = nodes[i];
-                for (int j = i + 1; j < nodes.length; j++) {
-                    DraftNode target = nodes[j];
-
-                    if (random.nextDouble() < wiringProbability && source != target) {
-                        int type = 0;
-                        if (typeCount > 1) {
-                            type = random.nextInt(typeCount);
-                        }
-                        DraftEdge edge = new DraftEdge(i, j, type, 1.0);
-                        edgeSet.add(edge);
-                        if (edgeSet.size() >= edgeCount) {
-                            break;
-                        }
-                    }
+            DraftNode first = nodes[random.nextInt(nodes.length)];
+            DraftNode second = nodes[random.nextInt(nodes.length)];
+            if (first != second) {
+                int type = 0;
+                if (typeCount > 1) {
+                    type = random.nextInt(typeCount);
                 }
-                if (edgeSet.size() >= edgeCount) {
-                    break;
-                }
+                DraftEdge edge = new DraftEdge(first.getId(), second.getId(), type, 1.0);
+                edgeSet.add(edge);
             }
         }
         return edgeSet.toArray(new DraftEdge[0]);
@@ -91,6 +83,11 @@ public class Generator {
         return this;
     }
 
+    public Generator withPositions() {
+        this.withPositions = true;
+        return this;
+    }
+
     public GraphStore build() {
         GraphModelImpl model = new GraphModelImpl(this.configuration);
         GraphFactory factory = model.factory();
@@ -100,8 +97,15 @@ public class Generator {
             return store;
         }
 
+        Random rand = new Random();
         for (DraftNode draftNode : nodes) {
-            store.addNode(factory.newNode(draftNode.getId()));
+            Node node = factory.newNode(draftNode.getId());
+            if (withPositions) {
+                float x = MINIMUM_POSITION + rand.nextFloat() * (MAXIMUM_POSITION - MINIMUM_POSITION);
+                float y = MINIMUM_POSITION + rand.nextFloat() * (MAXIMUM_POSITION - MINIMUM_POSITION);
+                node.setPosition(x, y);
+            }
+            store.addNode(node);
         }
 
         if (!withEdges) {
--- src/test/java/org/gephi/graphstore/benchmark/GeneratorTest.java
+++ src/test/java/org/gephi/graphstore/benchmark/GeneratorTest.java
@@ -1,6 +1,7 @@
 package org.gephi.graphstore.benchmark;
 
 
+import org.gephi.graph.api.Node;
 import org.gephi.graph.impl.GraphStore;
 import org.gephi.graphstore.benchmark.util.Generator;
 import org.junit.Test;
@@ -30,5 +31,19 @@ public class GeneratorTest {
         Generator generator = Generator.generate(100, 200);
         GraphStore store = generator.build();
         assert store.getEdgeCount() == 200;
+    }
+
+    @Test
+    public void testWithPositions() {
+        Generator generator = Generator.generate(100, 200).withPositions();
+        GraphStore store = generator.build();
+        for (Node node : store.getNodes()) {
+            float x = node.x();
+            float y = node.y();
+            assert x != 0;
+            assert y != 0;
+            assert x >= Generator.MINIMUM_POSITION && x <= Generator.MAXIMUM_POSITION;
+            assert y >= Generator.MINIMUM_POSITION && y <= Generator.MAXIMUM_POSITION;
+        }
     }
 }
